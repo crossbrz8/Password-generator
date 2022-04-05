52606c4ad85b24b3c4f0cda9dbc09021b1855870
crossbrz8 <59404958+crossbrz8@users.noreply.github.com>
2022-04-04 21:57:57 -0300

    Update index.js

--- password generator/index.js
+++ password generator/index.js
@@ -1,14 +1,12 @@
 var passwordEl = document.getElementById("gen-But")
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var passwordLength = 12
 var rectEl = document.getElementById("Rect1")
 var rectEl2 = document.getElementById("Rect2")
 var rectEl3 = document.getElementById("Rect3")
 var rectEl4 = document.getElementById("Rect4")
 var passwordArray = []
 var password = ""
-passwordEl.addEventListener('click', function() { // no evento click 
-  generatePassword('c'); // executa a função a() passando o tipo do metodo que é c();
+passwordEl.addEventListener('click', function() { 
+  generatePassword('c'); 
 }, false);
 
 
@@ -23,7 +21,8 @@ function generatePassword()
 {
     for(let i = 0; i < 4 ;i++)
     {
-       passwordArray[i] = genPass()     
+       passwordArray[i] = genPass()
+     
     }
     rectEl.textContent = passwordArray[0]
     rectEl2.textContent = passwordArray[1]
